f0577297c2f011c343fe5693ccb5203389206be5
Justin Yeo <justin.yeo@windriver.com>
2022-12-16 16:23:06 -0800

    Fixed loop keyword

--- services/db/goose/migrations/tkdb/20221213182528_simple_copy.sql
+++ services/db/goose/migrations/tkdb/20221213182528_simple_copy.sql
@@ -10,8 +10,8 @@ DECLARE
     _file file_table$ROWTYPE;
 BEGIN
     FOR _file IN SELECT * FROM file_table ORDER BY id
-        INSERT INTO file (sha256, file_size, md5, sha1) VALUES (_file.checksum_sha256, _file.size, _file.checksum_md5, _file.checksum_sha1);
     LOOP
+        INSERT INTO file (sha256, file_size, md5, sha1) VALUES (_file.checksum_sha256, _file.size, _file.checksum_md5, _file.checksum_sha1);
     END LOOP;
 END;
 $$;
@@ -25,13 +25,13 @@ DECLARE
     _sha1 BYTEA;
 BEGIN
     FOR _file IN SELECT * FROM file_alias_table ORDER BY id
+    LOOP
         SELECT _file.checksum_sha1 
         FROM file_table 
         WHERE file_table.id=_file_alias.id 
         INTO _sha1;
         
         INSERT INTO file_alias (file_sha256, name) VALUES (_file.checksum_sha256, _file.size, _file.checksum_md5, _file.checksum_sha1);
-    LOOP
     END LOOP;
 END;
 $$;
