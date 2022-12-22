8154b76a98b1048cc0dd7308a0ce0e4c914ef618
Bradley Prieskorn <bradley.prieskorn@windriver.com>
2022-12-22 15:00:00 -0500

    update search result store type safety
    Signed-off-by: Bradley Prieskorn <bradley.prieskorn@windriver.com>

--- services/nginx/frontend/src/stores/search.ts
+++ services/nginx/frontend/src/stores/search.ts
@@ -1,9 +1,36 @@
 import {defineStore} from "pinia"
 import {ref, Ref} from "vue"
 
+// ## Graphql Schema Definition
+// type Archive {
+//     id: Int64!
+//     file_collection_id: Int64
+//     file_collection: FileCollection
+//     name: String
+//     path: String
+//     size: Int64
+//     sha1: String
+//     sha256: String
+//     md5: String
+//     insert_date: Time!
+//     extract_status: Int!
+//   }
+type SearchResult = {
+    archive: {
+        extract_status: number
+        file_collection_id: number
+        id: number
+        insert_date: string
+        name: string
+        sha1: string
+        sha256: string
+    }
+    distance: number
+}
+
 export const useSearchStore = defineStore("search", () => {
-    const results: Ref<any[]> = ref([])
-    function setResults(values: any[]) {
+    const results: Ref<SearchResult[]> = ref([])
+    function setResults(values: SearchResult[]) {
         results.value = values
     }
     return { results, setResults }
