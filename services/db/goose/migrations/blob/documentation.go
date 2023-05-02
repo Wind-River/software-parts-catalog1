72f587206e9caabbf786af52b10ec16c621ad3b6
Justin Yeo <justin.yeo@windriver.com>
2023-05-02 02:25:25 -0700

    Changed tkdb -> catalog and reduced the number of versions

---
+++ services/db/goose/migrations/blob/documentation.go
@@ -0,0 +1,2 @@
+// blob contains migrations to set-up or update a blob database we use to keep store metadata on files and archives we keep long-term in object storage.
+package blob
---
+++ services/db/goose/migrations/catalog/documentation.go
@@ -0,0 +1,2 @@
+// catalog contains migrations to set-up or update the Software Parts Catalog database.
+package catalog
